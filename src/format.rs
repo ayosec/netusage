f9654f805cc0703e7893540ad0dc4dc7ea0e72b4
Ayose <ayosec@gmail.com>
2020-05-11 07:31:32 +0100

    Adjust format for speed values
    - Truncate values for KB/s
    - Hide B/s under "<1 KB/s"

--- src/format.rs
+++ src/format.rs
@@ -1,9 +1,14 @@
 pub fn speed(speed: f64) -> String {
-    let items = [(1024 * 1024 * 1024, "G"), (1024 * 1024, "M"), (1024, "K")];
+    let items = [
+        (1024 * 1024 * 1024, "G", 1),
+        (1024 * 1024, "M", 1),
+        (1024, "K", 0),
+    ];
 
-    match items.iter().find(|(d, _)| speed > *d as f64) {
-        Some((d, suffix)) => format!("{:.1} {}B/s", speed / *d as f64, suffix),
-        None => format!("{:.0} B/s", speed),
+    match items.iter().find(|(d, _, _)| speed > *d as f64) {
+        Some((d, suffix, prec)) => format!("{:.2$} {}B/s", speed / *d as f64, suffix, prec),
+        None if speed > 0.0 => String::from("<1 KB/s"),
+        None => String::from("0 KB/s"),
     }
 }
 
@@ -11,6 +16,7 @@ pub fn speed(speed: f64) -> String {
 fn format_speed() {
     assert_eq!(speed(3250585.6), String::from("3.1 MB/s"));
     assert_eq!(speed(9019431321.6), String::from("8.4 GB/s"));
-    assert_eq!(speed(5000.0), String::from("4.9 KB/s"));
-    assert_eq!(speed(1.0), String::from("1 B/s"));
+    assert_eq!(speed(5000.0), String::from("5 KB/s"));
+    assert_eq!(speed(100.0), String::from("<1 KB/s"));
+    assert_eq!(speed(0.0), String::from("0 KB/s"));
 }
